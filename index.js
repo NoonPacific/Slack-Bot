2f2fe3f7371838f3bdd69bdefb8aceebc038cf28
Jake Runzer <jakerunzer@gmail.com>
2016-04-20 05:26:37 -0700

    testing beepboop rtm

---
+++ index.js
@@ -0,0 +1,30 @@
+// example
+'use strict'
+
+var Botkit = require('botkit');
+var BeepBoop = require('beepboop-botkit');
+
+var controller = Botkit.slackbot();
+
+Beepboop.start(controller, {
+    debug: true
+});
+
+// Listen for botkit events
+controller.on('bot_channel_join', function(bot, message) {
+    bot.reply(message, 'I\'m here!');
+});
+
+controller.hears(['hi'], ['direct_message', 'direct_mention'], function(bot, evt) {
+    bot.reply(evt, 'hello from bot.js');
+});
+
+controller.hears(['config'], ['direct_message', 'direct_mention'], function(bot, evt) {
+    bot.reply(evt, 'CUSTOM_CONFIG_ITEM: ' + bot.config.CUSTOM_CONFIG_ITEM)
+});
+
+controller.hears(['help'], ['direct_message', 'direct_mention'], function(bot, evt) {
+    var help = 'I am an efficient little bot. All you need is a single instance of me to handle ' +
+        'multiple teams. If you run me on BeepBoop, I will even auto-scale! :boom:'
+    bot.reply(evt, help)
+});
